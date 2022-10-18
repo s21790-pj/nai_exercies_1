89eba746e21073506bf4656bd74609d8fb5c9704
donkamileq <donkamileq@gmail.com>
2022-10-18 18:41:08 +0200

    Try to Fix random number

--- bulls_and_cows.py
+++ bulls_and_cows.py
@@ -11,26 +11,37 @@ class BullsAndCowsProblem(SearchProblem):
 
     def actions(self, state):
         print(f'here in actions | state {state}')
+        print(f'self.bulls {self.bulls}')
+
         if state != GOAL:
-            return list('0123456789')
-        else:
-            return []
+            for num in self.bulls:
+                if num in self.cows:
+                    print('HERE')
+                    self.cows.pop(self.cows.index(num))
+            if self.cows:
+                return self.cows
+            else:
+                print('HERE')
+                chosen_numer = random.randint(0, 9)
+
+                while chosen_numer in self.bulls:
+                    chosen_numer = random.randint(0, 9)
+                return str(chosen_numer)
 
     def result(self, state, action):
+        state = self.bulls
         print(f'here in result | state {state}')
+        print(f"action: {action}")
         print(f'self.bulls {self.bulls}')
         print(f'self.cows {self.cows}')
-        state = self.bulls
         temp_cows = self.cows
 
         for it in range(len(state)):
-            if state[it] == 'x':
-                if temp_cows:
-                    chosen_number = random.choice(temp_cows)
-                    state[it] = chosen_number
-                    temp_cows.pop(temp_cows.index(chosen_number))
-                else:
-                    state[it] = action
+            chosen_numer = random.choice(action)
+            if self.bulls[it] == 'x':
+                self.bulls[it] = chosen_numer
+
+        state = self.bulls
 
         return ''.join(state)
 
@@ -41,31 +52,32 @@ class BullsAndCowsProblem(SearchProblem):
     def heuristic(self, state):
         print(f'here in heuristic | state {state}')
 
-        for it in range(len(state)):
+        for it in range(len(GOAL)):
+            if (state[it] in GOAL) and (state[it] not in self.cows):
+                self.cows.append(state[it])
+
+        for it in range(len(GOAL)):
             if state[it] == GOAL[it]:
                 self.bulls[it] = state[it]
             else:
                 self.bulls[it] = 'x'
+
         state = self.bulls
-
-        for it in range(len(GOAL)):
-            if (state[it] in GOAL) and (state[it] not in self.cows) and (state[it] not in self.bulls):
-                self.cows.append(state[it])
-
         wrong = sum([0 if state[i] != 'x' else 1 for i in range(len(state))])
         print(f"wrong: {wrong}")
         return wrong
 
 
 def generate_num_v2():
-    # while True:
+    while True:
         num = random.randint(1000, 9999)
         numbers = list(str(num))
         if len(numbers) == len(set(numbers)):
             return str(num)
 
 
-problem = BullsAndCowsProblem(initial_state=generate_num_v2())
+# problem = BullsAndCowsProblem(initial_state=generate_num_v2())
+problem = BullsAndCowsProblem(initial_state='4675')
 result = astar(problem)
 
 print(result.state)
