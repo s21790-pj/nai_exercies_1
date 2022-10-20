b2282f63735eefee8ab72270c9752acc3ab6dc78
donkamileq <donkamileq@gmail.com>
2022-10-20 18:16:57 +0200

    Fix the logic of the game and the selection of random numbers

--- bulls_and_cows.py
+++ bulls_and_cows.py
@@ -7,54 +7,75 @@ GOAL = "7564"
 
 class BullsAndCowsProblem(SearchProblem):
     bulls = ['x' for x in range(len(GOAL))]
-    cows = []
+    cows = ['']
 
     def actions(self, state):
-        print(f'here in actions | state {state}')
-        print(f'self.bulls {self.bulls}')
+        """
 
+        :param state:
+        :return:
+        """
+        missing_numbers = ''
         if state != GOAL:
             for num in self.bulls:
                 if num in self.cows:
-                    print('HERE')
                     self.cows.pop(self.cows.index(num))
-            if self.cows:
+            if self.cows[0] != '':
+                temp_cows = ''.join(self.cows)
+                self.cows[0] = temp_cows
+                for missing_number in range(self.bulls.count('x') - len(temp_cows)):
+                    chosen_number = str(random.randint(0, 9))
+                    while (chosen_number in self.bulls) or (chosen_number in temp_cows):
+                        chosen_number = str(random.randint(0, 9))
+                    missing_numbers += chosen_number
+                temp_cows += missing_numbers
+                self.cows[0] = temp_cows
                 return self.cows
             else:
-                print('HERE')
-                chosen_numer = random.randint(0, 9)
-
-                while chosen_numer in self.bulls:
-                    chosen_numer = random.randint(0, 9)
-                return str(chosen_numer)
+                missing_numbers = ''
+                for it in range((self.bulls.count('x'))):
+                    chosen_number = str(random.randint(0, 9))
+                    while chosen_number in self.bulls or chosen_number in missing_numbers:
+                        chosen_number = str(random.randint(0, 9))
+                    missing_numbers += chosen_number
+                return [missing_numbers]
 
     def result(self, state, action):
+        """
+
+        :param state:
+        :param action:
+        :return:
+        """
+        # 4
         state = self.bulls
-        print(f'here in result | state {state}')
-        print(f"action: {action}")
-        print(f'self.bulls {self.bulls}')
-        print(f'self.cows {self.cows}')
-        temp_cows = self.cows
+        temp_cows = list(action)
 
-        for it in range(len(state)):
-            chosen_numer = random.choice(action)
-            if self.bulls[it] == 'x':
-                self.bulls[it] = chosen_numer
+        for it in range((self.bulls.count('x'))):
+            chosen_number = random.choice(temp_cows)
+            temp_cows.pop(temp_cows.index(chosen_number))
+            self.bulls[self.bulls.index('x')] = chosen_number
 
+        self.cows = temp_cows
         state = self.bulls
-
         return ''.join(state)
 
     def is_goal(self, state):
-        print(f'here in is_goal | state {state}')
+        """
+
+        :param state:
+        :return:
+        """
         return state == GOAL
 
     def heuristic(self, state):
-        print(f'here in heuristic | state {state}')
+        """
 
-        for it in range(len(GOAL)):
-            if (state[it] in GOAL) and (state[it] not in self.cows):
-                self.cows.append(state[it])
+        :param state:
+        :return:
+        """
+        self.cows = ['']
+        result = ''
 
         for it in range(len(GOAL)):
             if state[it] == GOAL[it]:
@@ -62,13 +83,21 @@ class BullsAndCowsProblem(SearchProblem):
             else:
                 self.bulls[it] = 'x'
 
+        for it in range(len(GOAL)):
+            if (state[it] in GOAL) and (state[it] not in self.bulls):
+                result += str(state[it])
+            self.cows[0] = result
+
         state = self.bulls
         wrong = sum([0 if state[i] != 'x' else 1 for i in range(len(state))])
-        print(f"wrong: {wrong}")
         return wrong
 
 
 def generate_num_v2():
+    """
+
+    :return:
+    """
     while True:
         num = random.randint(1000, 9999)
         numbers = list(str(num))
@@ -76,8 +105,7 @@ def generate_num_v2():
             return str(num)
 
 
-# problem = BullsAndCowsProblem(initial_state=generate_num_v2())
-problem = BullsAndCowsProblem(initial_state='4675')
+problem = BullsAndCowsProblem(initial_state=generate_num_v2())
 result = astar(problem)
 
 print(result.state)
