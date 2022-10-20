22b4bfc064d2fff2cd0120d584141c7b0287a7a2
donkamileq <donkamileq@gmail.com>
2022-10-20 18:52:49 +0200

    Add documentation

--- bulls_and_cows.py
+++ bulls_and_cows.py
@@ -1,19 +1,25 @@
 import random
 
+from typing import List
 from simpleai.search import SearchProblem, astar
 
-GOAL = "7564"
+# Answer to the task
+GOAL = "1234"
 
 
 class BullsAndCowsProblem(SearchProblem):
+    # Initialization of bull score and cow score tables
     bulls = ['x' for x in range(len(GOAL))]
     cows = ['']
 
-    def actions(self, state):
+    def actions(self, state: str) -> List:
         """
-
-        :param state:
-        :return:
+        The method responsible for verifying the current result, and selecting missing numbers.
+        The method first selects for missing numbers those that are contained in the cows.
+        If there are more missing numbers than cows, the numbers are selected randomly
+        so that they are not present in bulls and cows.
+        :param state: Current answer in the form of a 4-digit number written as a string
+        :return: One-element array, containing the missing drawn numbers in string form
         """
         missing_numbers = ''
         if state != GOAL:
@@ -40,14 +46,14 @@ class BullsAndCowsProblem(SearchProblem):
                     missing_numbers += chosen_number
                 return [missing_numbers]
 
-    def result(self, state, action):
+    def result(self, state: str, action: str) -> str:
         """
-
-        :param state:
+        The method responsible for creating a new result by randomly
+        substituting in the missing places the numbers obtained from the action method.
+        :param state: Current answer in the form of a 4-digit number written as a string
         :param action:
         :return:
         """
-        # 4
         state = self.bulls
         temp_cows = list(action)
 
@@ -60,19 +66,22 @@ class BullsAndCowsProblem(SearchProblem):
         state = self.bulls
         return ''.join(state)
 
-    def is_goal(self, state):
+    def is_goal(self, state: str) -> bool:
         """
-
-        :param state:
-        :return:
+        A method that verifies the correctness of the current answer with the expected result.
+        :param state: Current answer in the form of a 4-digit number written as a string
+        :return: True if the answer and the result are the same otherwise False
         """
         return state == GOAL
 
-    def heuristic(self, state):
+    def heuristic(self, state: str) -> int:
         """
-
-        :param state:
-        :return:
+        The method responsible for verifying the state of the answer.
+        In the method, the number of cows and bulls is obtained.
+        Also, the number of incorrect digits in the result is determined
+        by verifying the number of 'x' in the bulls array.
+        :param state: Current answer in the form of a 4-digit number written as a string
+        :return: Number of incorrect digits in the current answer
         """
         self.cows = ['']
         result = ''
@@ -93,10 +102,10 @@ class BullsAndCowsProblem(SearchProblem):
         return wrong
 
 
-def generate_num_v2():
+def generate_num_v2() -> str:
     """
-
-    :return:
+    The method responsible for generating a random number as the initial answer to a task.
+    :return: A number consisting of 4 random, non-repeating digits
     """
     while True:
         num = random.randint(1000, 9999)
